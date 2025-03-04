70a044631e4557b2b88aca98be4dd0e0ad162355
darco512 <darcohaha@gmail.com>
2025-03-04 21:34:04 +0200

    add route to cleare db before test

---
+++ src/input-output-types/input-validation.ts
@@ -0,0 +1,66 @@
+import {OutputErrorsType} from "./output-errors-type";
+import {Resolutions} from "./video-types";
+
+export const tittleFieldValidator = (title: string, errors: OutputErrorsType) => {
+
+    if(!title){
+        errors.errorsMessages.push({
+            message: 'No title', field: 'title',
+        })
+    }
+
+    if(title && title.trim().length > 40) {
+        errors.errorsMessages.push({message:'More then 40 symbols', field: 'title'})
+    }
+
+    if(title && title.trim().length < 1) {
+        errors.errorsMessages.push({
+            message: 'No title', field: 'title',
+        })
+    }
+}
+
+export const authorFieldValidator = (author: string, errors: OutputErrorsType) => {
+
+    if(!author){
+        errors.errorsMessages.push({
+            message: 'No title', field: 'title',
+        })
+    }
+
+    if(author && author.trim().length > 20) {
+        errors.errorsMessages.push({message:'More then 20 symbols', field: 'title'})
+    }
+
+    if(author && author.trim().length < 1) {
+        errors.errorsMessages.push({
+            message: 'No title', field: 'title',
+        })
+    }
+}
+
+export const availableResolutionFieldValidator = (availableResolution: Resolutions[], errors: OutputErrorsType) => {
+    if (!Array.isArray(availableResolution)
+        || availableResolution.find(p => !Resolutions[p])
+    ) {
+        errors.errorsMessages.push({
+            message: 'At least one resolution should be added', field: 'availableResolution'
+        })
+    }
+}
+
+export const ageRestrictionFieldValidator = (minAgeRestriction: number, errors: OutputErrorsType) => {
+    if(minAgeRestriction && (minAgeRestriction > 18 || minAgeRestriction < 1)) {
+        errors.errorsMessages.push({
+            message: 'Age restriction can\'t be less then 1 and more then 18' , field: 'minAgeRestriction'
+        })
+    }
+}
+
+export const canBeDownloadedFieldValidator = (canBeDownloaded: boolean, errors: OutputErrorsType) => {
+    if (!canBeDownloaded) {
+        errors.errorsMessages.push({
+            message: 'No information', field: 'canBeDownloaded'
+        })
+    }
+}
--- src/videos/index.ts
+++ src/videos/index.ts
@@ -1,28 +1,129 @@
 
 import {Request, Response, Router} from 'express'
-import {getVideosController} from './getVideosController'
-import {createVideoController} from './createVideoController'
 import {db} from "../db/db";
-import {findVideoController} from './findVideoController'
-import {updateVideoController} from './updateVideoController'
-import {deleteVideoController} from './deleteVideoController'
+import {OutputVideoType} from "../input-output-types/video-types";
+import {OutputErrorsType} from "../input-output-types/output-errors-type";
+import {
+    ageRestrictionFieldValidator,
+    authorFieldValidator,
+    availableResolutionFieldValidator, canBeDownloadedFieldValidator,
+    tittleFieldValidator
+} from "../input-output-types/input-validation";
 
 export const videosRouter = Router()
 
 const videoController = {
-    getVideosController: getVideosController,
-    createVideoController: createVideoController,
-    findVideoController: findVideoController,
-    updateVideoController: updateVideoController,
-    deleteVideoController: deleteVideoController
+    getVideos (req: Request, res: Response) {
+        const videos: OutputVideoType[] = db.videos // получаем видео из базы данных
+        res.status(200).json(videos) // отдаём видео в качестве ответа
+    },
+
+    createVideo (req: Request, res: Response) {
+        const title = req.body.title
+        const author = req.body.author
+        const availableResolution = req.body.availableResolution
+        const errors :OutputErrorsType = {
+            errorsMessages : []
+        }
+        tittleFieldValidator(title, errors)
+        authorFieldValidator(author, errors)
+        availableResolutionFieldValidator(availableResolution, errors)
+
+        if (errors.errorsMessages.length) { // если есть ошибки - отправляем ошибки
+            res
+                .status(400)
+                .json(errors)
+            return
+        }
+
+        const newVideo: OutputVideoType = {
+            ...req.body,
+            id: Date.now() + Math.random(),
+            title: req.body.title,
+            author: req.body.author,
+            canBeDownloaded: true,
+            minAgeRestriction: null,
+            createdAt: new Date().toISOString(),
+            publicationDate: new Date().toISOString(),
+            availableResolution: req.body.availableResolution,
+        }
+        db.videos = [...db.videos, newVideo]
+
+        res
+            .status(201)
+            .json(newVideo)
+    },
+
+    findVideo (req: Request, res: Response) {
+        const foundVideo = db.videos.find(c => c.id === +req.params.id)
+        if(!foundVideo){
+            res.sendStatus(404);
+            return
+        }
+
+        res
+            .status(200)
+            .json(foundVideo)
+    },
+
+    updateVideo (req: Request, res: Response) {
+        const foundVideo = db.videos.find(v => v.id === +req.params.id)
+
+        if(!foundVideo){
+            res.sendStatus(404);
+            return
+        }
+
+        const title = req.body.title
+        const author = req.body.author
+        const canBeDownloaded = req.body.canBeDownloaded
+        const minAgeRestriction = req.body.minAgeRestriction
+        const availableResolution = req.body.availableResolution
+        const errors :OutputErrorsType = {
+            errorsMessages : []
+        }
+
+        tittleFieldValidator(title, errors)
+        authorFieldValidator(author, errors)
+        availableResolutionFieldValidator(availableResolution, errors)
+        ageRestrictionFieldValidator(minAgeRestriction, errors)
+        canBeDownloadedFieldValidator(canBeDownloaded,errors)
+
+
+        if (errors.errorsMessages.length) { // если есть ошибки - отправляем ошибки
+            res
+                .status(400)
+                .json(errors)
+            return
+        }
+
+
+        foundVideo.title = title
+        foundVideo.author = author
+        foundVideo.canBeDownloaded = canBeDownloaded
+        foundVideo.minAgeRestriction = minAgeRestriction
+        foundVideo.publicationDate = new Date().toISOString()
+        foundVideo.availableResolution = availableResolution
+
+        res
+            .status(204)
+    },
+
+    deleteVideo (req: Request, res: Response) {
+        const foundVideo =db.videos.find(v => v.id === +req.params.id)
+        if(!foundVideo){
+            res.sendStatus(404);
+            return
+        }
+
+        db.videos = db.videos.filter(v => v.id !== req.params.id)
+        res.status(204)
+    }
 }
 
-videosRouter.get('/', videoController.getVideosController)
-videosRouter.post('/', videoController.createVideoController)
-videosRouter.get('/:id', videoController.findVideoController)
-videosRouter.put('/:id', videoController.updateVideoController)
-videosRouter.delete('/:id', videoController.deleteVideoController)
+videosRouter.get('/', videoController.getVideos)
+videosRouter.post('/', videoController.createVideo)
+videosRouter.get('/:id', videoController.findVideo)
+videosRouter.put('/:id', videoController.updateVideo)
+videosRouter.delete('/:id', videoController.deleteVideo)
 
-// ...
-
-// не забудьте добавить роут в апп
